d8259c92c5fbf1500c7699bc2848cb47f540075e
Ameen-pv-xyz <ameenpvpdkl@gmail.com>
2024-08-28 02:51:14 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+function sendMail() {
+    var params = {
+        name:document.getElementById("name").value ,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+        
+    };
+
+
+    const serviceID = "service_efp1jzi";
+const templateID = "template_c5byztd";
+
+emailjs.send(serviceID,templateID,params)
+.then (
+    (res )=> {
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("your message send successfully");
+        
+    })
+
+    .catch((err) => console.log(err));
+}
+
+
